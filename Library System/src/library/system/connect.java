6b914fd903f6d2eaeb8af05e9a8e0e60646d8320
Mishan Madhupa <mishancool2016@gmail.com>
2020-07-21 15:05:24 +0530

    Create login front end

--- Library System/src/library/system/connect.java
+++ Library System/src/library/system/connect.java
@@ -9,6 +9,22 @@ package library.system;
  *
  * @author MISHAN
  */
+
+import java.sql.*;
+
 public class connect {
+    
+    Connection c;
+    Statement s;
+    
+    public connect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","");
+            s = c.createStatement();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
     
 }
